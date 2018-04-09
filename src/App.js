041ef2db65e86a074c480d0233a609f8c6cf4bc9
quanuw <quanuw@gmail.com>
2018-04-08 20:24:22 -0700

    start search function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
-DEFAULT_QUERY = ''
+const DEFAULT_QUERY = 'wtf';
+
+const PATH_BASE = 'http://api.songkick.com/api/3.0/';
+const PATH_ARTISTS = 'artists/';
+const PATH_FORMAT = 'calendar.json';
+const PATH_API_KEY= 'apikey=';
+const API_KEY = '';
+// const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`;
+
 class App extends Component {
 
   constructor(props) {
@@ -11,14 +20,59 @@ class App extends Component {
     this.state = {
       results: null,
       searchTerm: DEFAULT_QUERY,
+      error: null,
     }
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.fetchTours = this.fetchTours.bind(this);
   }
 
-  this.onSearchChange = this.onSearchChange.bind(this);
-  this.onSearchSubmit = this.onSearchSubmit.bind(this);
+  fetchTours(searchTerm) {
+    axios(`${PATH_BASE}${PATH_ARTISTS}${searchTerm}/${PATH_FORMAT}?${PATH_API_KEY}${API_KEY}`) 
+      .then(result => this.setSearchTopStories(result.data))
+      .catch(error => this.setState({ error }));
+      console.log(searchTerm);
+      console.log(this._isMounted);
+  }
 
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.fetchTours(searchTerm);
+    event.preventDefault();
+  }
+
+  setTours(result) {
+    const { resultPage } = result;
+
+    const oldResultPages = this.state.results !== null
+      ? this.state.results
+      : []
+
+    const updatedResultsPages = [
+      ...oldResultPages,
+      ...resultPage
+    ];
+
+    this.setState({
+      results: updatedResultsPages
+    })
+  }
   
+
+
   render() {
+
+    const { 
+      searchTerm, 
+      results,
+      error,
+    } = this.state;
+
     return (
       <div className="App">
         <div className="interactions">
@@ -34,5 +88,30 @@ class App extends Component {
     );
   }
 }
+
+const Search = ({
+  value,
+  onChange,
+  onSubmit,
+  children
+}) =>
+  <form onSubmit={onSubmit}> 
+    <input 
+      type="text" 
+      value={value} 
+      onChange={onChange} 
+    /> 
+    <button type="submit"> 
+      {children} 
+    </button> 
+  </form>
+
+  Search.PropTypes = {
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired,
+  }
+
 
 export default App;
