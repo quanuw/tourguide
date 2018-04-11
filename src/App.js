1c5b1224e16d6a5dd7b735a7829bbdc4ffaa72f3
quanuw <quanuw@gmail.com>
2018-04-10 23:39:09 -0700

    add styling for Cards

--- src/App.js
+++ src/App.js
@@ -41,6 +41,8 @@ class App extends Component {
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.fetchTours = this.fetchTours.bind(this);
+    this.stringToHash = this.stringToHash.bind(this);
+    this.hashToRGB = this.hashToRGB.bind(this);
   }
 
   fetchTours(searchTerm) {
@@ -77,7 +79,24 @@ class App extends Component {
       results: updatedResultsPages
     })
   }
+
+  stringToHash(s) {
+    let hash = 5381;
+    for (let i = 0; i < s.length; i++) {
+      hash = ((hash << 5) + hash) + s.charCodeAt(i);
+    }
+    return hash;
+  }
+
+  hashToRGB(s) {
+    let hash = this.stringToHash(s);
+    let r = (hash & 0xFF0000) >> 16;
+    let g = (hash & 0x00FF00) >> 8;
+    let b = hash & 0x0000FF;
+    return "#" + ("0" + r.toString(16)).substr(-2) + ("0" + g.toString(16)).substr(-2) + ("0" + b.toString(16)).substr(-2);
+  }
   
+
 
 
   render() {
@@ -98,7 +117,11 @@ class App extends Component {
           > 
             Search
           </Search>
-          <Cards/>
+          <Cards 
+            hashFunc={this.stringToHash}
+            RGBFunc={this.hashToRGB}
+          >
+          </Cards>
         </div>
       </div>
     );
@@ -129,22 +152,42 @@ const Search = ({
     children: PropTypes.node.isRequired,
   }
 
+// TODO: How to change background color based on venue hash?
 const Cards = ({
+  hashFunc,
+  RGBFunc,
+}) => {
+  return (
+    <div className="cards">
+    {list.map(item => 
 
-}) =>
-  <div className="cards">
-    {list.map(item =>
-      <div key={item.objectID} className="card">
+      <div key={item.objectID} className="card" style={{RGBFunc({item.venue})}}>
         <div className="card-venue">
           <span className="venue">{item.venue}</span>
         </div>
         <div class="card-info">
-          <span>{item.date}</span>
-          <span>{item.price}</span>
+          <span className="date">DATE: {item.date}</span>
+          <span className="price">PRICE: {item.price}</span>
+          <span className="buy-link">
+            <a href="https://www.songkick.com/">BUY TICKET</a>
+          </span>
         </div>
       </div>
-    )}
+    )};
   </div>
+
+  // Cards.PropTypes = {
+  //   hashFunc: PropTypes.func,
+  //   RGBFunc: PropTypes.func,
+  // }
+  );
+
+
+
+}
+  
+
+
 
 
   
